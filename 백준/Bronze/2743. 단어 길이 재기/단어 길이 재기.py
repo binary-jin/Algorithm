874af6efc5e7130cc3489650ed6209752cb54bd8
박이진 <75563055+binary-jin@users.noreply.github.com>
2024-08-15 23:38:46 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.py
@@ -0,0 +1,14 @@
+#2024-08-15
+#1. 알파벳으로만 된 단어를 입력 받기
+a = input()
+
+#2. 한글자씩 리스트에 넣어서 새로운 리스트를 만듬
+#list(a)
+#for i in a:
+#   list.append(a)
+#list.a
+list_new = list(a)
+
+#3. 리스트의 크기를 출력
+#list_new.len()
+print(len(list_new))
